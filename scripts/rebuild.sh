07dd603e8018f14730fd8fc8a2610d0e427615fd
Igor Kim @ HfTL <gs2016007@hft-leipzig.de>
2018-11-29 13:49:02 +0100

    update script to correctly handle rebuilding

--- scripts/rebuild.sh
+++ scripts/rebuild.sh
@@ -3,5 +3,11 @@ CONTAINER=${1:-srsall_dev}
 SERVICE=${2:-srsall}
 SRC=${3:-./}
 
-docker cp $SRC $CONTAINER:/$SERVICE
-docker exec $CONTAINER /bin/sh -c "cd /$SERVICE/build && make"
+echo Preserving build folder
+docker exec -w / $CONTAINER /bin/sh -c "mv /$SERVICE/build /tmp_build"
+docker exec -w / $CONTAINER /bin/sh -c "rm -rf /$SERVICE"
+echo Copying files from $3 to $1:/$2
+docker cp $SRC/. $CONTAINER:/$SERVICE
+echo Move build folder back
+docker exec -w / $CONTAINER /bin/sh -c "rm -rf /$SERVICE/build && mv /tmp_build /$SERVICE/build"
+docker exec -w /$SERVICE/build $CONTAINER /bin/sh -c "make"
