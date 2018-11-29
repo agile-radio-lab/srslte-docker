2730540674ddf131f2e35b951a235ecb280420d1
Igor Kim @ HfTL <gs2016007@hft-leipzig.de>
2018-11-29 14:29:47 +0100

    add parameters for the service run

--- scripts/run.sh
+++ scripts/run.sh
@@ -3,5 +3,6 @@ CONTAINER=${1:-srsall_dev}
 APP=${2:-srsenb}
 CONF=${3:-srsenb.conf}
 SERVICE=${4:-srsall}
+PARAMS=${5:-}
 
-docker exec -it $CONTAINER /bin/sh -c "cd /$SERVICE/build/$APP/src && ./$APP /root/.srs/$CONF"
+docker exec -w /$SERVICE/build/$APP/src -it $CONTAINER /bin/sh -c "./$APP $PARAMS /root/.srs/$CONF"
