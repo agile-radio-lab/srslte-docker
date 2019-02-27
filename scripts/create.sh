eb10165341f2deed8766fadcc0ee6d8df47caaf8
Igor Kim @ HfTL <gs2016007@hft-leipzig.de>
2019-02-27 10:57:41 +0100

    make config path configurable

--- scripts/create.sh
+++ scripts/create.sh
@@ -4,9 +4,11 @@ IMAGE=${2:-srsall}
 DOCKERFILE=${3:-docker/srsall/Dockerfile}
 CONTEXT=${4:-.}
 TAG=${5:-latest}
+CONFIGPATHHOST=${6:-~/.srs}
+CONFIGPATH=${7:-/root/.config/srslte}
 
 docker kill $CONTAINER
 docker rm $CONTAINER
 
 docker build -t $IMAGE:$TAG -f $DOCKERFILE $CONTEXT
-docker run --name=$CONTAINER --network=host --privileged -di -v /lib/modules:/lib/modules -v ~/.srs:/root/.config/srslte $IMAGE:$TAG
+docker run --name=$CONTAINER --network=host --privileged -di -v /lib/modules:/lib/modules -v $CONFIGPATHHOST:$CONFIGPATH $IMAGE:$TAG
--- scripts/run.sh
+++ scripts/run.sh
@@ -4,5 +4,6 @@ APP=${2:-srsenb}
 CONF=${3:-srsenb.conf}
 SERVICE=${4:-srsall}
 PARAMS=${5:-}
+CONFIGPATH=${6:-/root/.config/srslte}
 
-docker exec -w /$SERVICE/build/$APP/src -it $CONTAINER /bin/sh -c "./$APP $PARAMS /root/.config/srslte/$CONF"
+docker exec -w /$SERVICE/build/$APP/src -it $CONTAINER /bin/sh -c "./$APP $PARAMS $CONFIGPATH/$CONF"
